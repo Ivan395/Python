021b8f98366e81f74d29712d4c5576fbee222409
Angel Ivan Carrillo de Leon <angelivan395@gmail.com>
2018-04-06 01:49:38 -0500

    La suma total del 1 hasta n y el factorial de n numero

---
+++ suma_y_factorial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+'''
+En este programa vamos a modificar para aprender a usar ramas de git.
+Let's go!!!!
+'''
+
+
+def factorial(num):
+    fact = 1
+    for i in range(1, num+1):
+        fact *= i
+    return fact
+
+
+def suma(num):
+    fact = 0
+    for i in range(1, num+1):
+        fact += i
+    return fact
+
+def run():
+    try:
+        fact = int(input('Ingresa un número: '))
+    except ValueError:
+        print('Ingresa un valor correcto, solo números enteros!')
+    res_suma = suma(fact)
+    res_fact = factorial(fact)
+    print('La suma de 1 hasta {} es {}'.format(fact, res_suma))
+    print('El factorial de {} es {}'.format(fact, res_fact))
+
+
+if __name__ == '__main__':
+    print('Suma de números')
+    run()
