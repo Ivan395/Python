841bf7e4597cc9d6d5c16599b5a5c0233965bad6
Angel Ivan Carrillo de Leon <angelivan395@gmail.com>
2018-04-06 01:48:44 -0500

    Descargar imágenes desde pexels

---
+++ pexels_web.py
@@ -0,0 +1,31 @@
+# !/usr/lib64/env python3.6
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+import requests
+from colorama import *
+URL = 'https://www.pexels.com/search/'
+
+
+def run():
+    search = input(Style.RESET_ALL + 'Search for free photos(The search must be in English): ')
+    url = '{}{}'.format(URL, search)
+    headers = {'User-Agent': 'Mozilla/5.0'}
+    web = requests.get(url, headers=headers)
+    soup = BeautifulSoup(web.content, 'html.parser')
+    urls_images = soup.find_all('img', attrs={'class': 'photo-item__img'})
+    for links in urls_images:
+        source_image = links.get('src')
+        image_url = source_image.split('?')[0]
+        # print(image_url)
+        image_name = image_url.split('/')[-1]
+        print('Downloading image {}'.format(image_name))
+        # print('image url: {}'.format(image_url))
+        with open('{}'.format(image_name), 'wb') as f:
+            tmp_url_image = requests.get(image_url, headers={'User-Agent': 'Mozilla/5.0.4'})
+            f.write(tmp_url_image.content)
+
+
+if __name__ == '__main__':
+    init()
+    print(Fore.GREEN + 'WELCOME TO MY PROGRAM TO DOWNLOAD IMAGES')
+    run()
