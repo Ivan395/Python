3dd20a09aaf0c9e3d687455e2b42eb3bf68a39ab
Angel Ivan Carrillo de Leon <angelivan395@gmail.com>
2018-04-08 22:37:50 -0500

    probando la clase celuar

---
+++ test_celular.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+# -*-coding: utf-8 -*-
+
+import celular
+
+def run():
+    per = celular.Celular("G7 Plus", "Samsung", "8442808637")
+    print(per.show())
+
+if __name__ == '__main__':
+    run()
