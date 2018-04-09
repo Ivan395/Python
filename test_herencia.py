c6eb75ae9796721833d506126f58c24d7c15d76c
Angel Ivan Carrillo de Leon <angelivan395@gmail.com>
2018-04-09 00:26:23 -0500

    Probando la herencia

---
+++ test_herencia.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+# -*- coding: utf-8 -*-
+from cuadrado import Cuadrado
+
+def run():
+    cuad = Cuadrado(1,2,3)
+    print(cuad.show())
+
+if __name__ == '__main__':
+    run()
